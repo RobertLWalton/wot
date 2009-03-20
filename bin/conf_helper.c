76cc039efc70a750b9bab6dea172d6070cbf3be5
root <root>
2009-03-20 12:53:02 +0000

    *** empty log message ***

--- bin/conf_helper.c
+++ bin/conf_helper.c
@@ -3,7 +3,7 @@
  *
  * File:	conf_passwd_shadow.c
  * Author:	Bob Walton (walton@deas.harvard.edu)
- * Date:	Thu Mar 19 11:42:21 EDT 2009
+ * Date:	Fri Mar 20 06:46:30 EDT 2009
  *
  * The authors have placed this program in the public
  * domain; they make no warranty and accept no liability
@@ -12,9 +12,9 @@
  * RCS Info (may not be true date or author):
  *
  *   $Author: root $
- *   $Date: 2009/03/19 17:24:08 $
+ *   $Date: 2009/03/20 12:53:02 $
  *   $RCSfile: conf_helper.c,v $
- *   $Revision: 1.2 $
+ *   $Revision: 1.3 $
  */
 
 #include <stdlib.h>
@@ -22,53 +22,113 @@
 #include <string.h>
 #include <assert.h>
 
+/* conf_passwd_shadow VERBOSE HOST TYPE COMMAND FILE \
+ *		      SOURCEFILE TARGETFILE TMPFILE
+ *
+ * VERBOSE:	0 or 1
+ * HOST		HOST variable value from `conf' program
+ * TYPE		passwd or shadow
+ * COMMAND	put or get
+ * FILE		name of file relative to directories
+ *		for error messages
+ * SOURCEFILE	source file name
+ * TARGETFILE	target file name
+ * TMPFILE	temporary file name provided by `conf'
+ *
 /* This program accepts the COMMANDs `put' and `get' and
- * copies the source or target files, repectively, to
- * to the tmpfile, making appropriate field substitu-
- * tions.  The file TYPEs are `passwd' or `shadow'
+ * copies the SOURCEFILE or TARGERFILE, repectively, to
+ * to the TMFILE, making appropriate field substitu-
+ * tions, according to the COMMAND and TYPE.
+ *
+ * See documentation of the `conf' program and the code
+ * of the passwd_conf and shadow_conf functions in that
+ * program.
  */
 
-/* Move a char pointer to the next `:' or NUL.
+/* Field pointer.
  */
-#define SKIP(p) while ( * p && * p != ':' ) ++ p; \
-                if ( * p ) * p ++ = 0
-
-/* Ditto but print field skipped over to tmpf preceeded
- * by a ':'.
- */
-#define PRINT(p) print ( tmpf, & p )
-void print ( FILE * tmpf, char ** pp )
+struct fpointer_struct
 {
-    char * p = * pp;
-    char * q = p;
-    SKIP ( p );
-    fprintf ( tmpf, ":%s", q );
-    * pp = p;
+    char * cp;		/* Points at field. */
+    int present;	/* True iff field present */
+    			/* (i.e., preceeded by ':' ); */
+};
+typedef struct fpointer_struct fpointer;
+
+/* Initialize a field pointer to a buffer pointer.
+ */
+void init ( fpointer * fpp, char * bufferp )
+{
+    fpp->cp = bufferp;
+    fpp->present = ( * bufferp != 0 );
 }
+#define INIT(fp,bufferp) init ( & fp, bufferp )
+
+/* Skip a field.  Set the char after the field to NUL.
+ */
+void skip ( fpointer * fpp )
+{
+    char * cp = fpp->cp;
+    if ( ! fpp->present ) return;
+    while ( * cp && * cp != ':' ) ++ cp;
+    fpp->present = ( * cp == ':' );
+    if ( fpp->present ) * cp ++ = 0;
+    fpp->cp = cp;
+}
+#define SKIP(fp) skip(&(fp))
+
+/* Ditto but print the field to tmpf preceded by a ':',
+ * if the field is present.
+ */
+FILE * tmpf;
+void print ( fpointer * fpp )
+{
+    char * cp = fpp->cp;
+    if ( ! fpp->present ) return;
+    skip ( fpp );
+    fprintf ( tmpf, ":%s", cp );
+}
+#define PRINT(fp) print(&(fp))
+
+/* Print the rest of the fields and an end of line.
+ */
+void printrest ( fpointer * fpp )
+{
+    while ( fpp->present ) PRINT ( * fpp );
+    fprintf ( tmpf, "\n" );
+}
+#define PRINTREST(fp) printrest ( &(fp) )
+
 
 /* List of file lines that contain information to be
  * inserted into the file being copied, indexed by
  * account name.
  */
-struct line
+struct line_struct
 {
     /* Line is malloced.  Account points at beginning
      * and is NUL terminated, the : having been
-     * replaced in the line by NUL.  rest points just
-     * after the NUL unless there is nothing after
-     * the NUL, in which it points to NUL.
+     * replaced in the line by NUL.  rest is a field
+     * pointer pointing at the first field after
+     * the account.
      */
 
     char * account;
-    char * rest;		
-    struct line * previous;
-} * lastline = NULL;
+    fpointer rest;		
+    struct line_struct * previous;
+};
+typedef struct line_struct line;
+
+/* Lines are changed together from lastline via previous
+ * member.
+ */
+line * lastline = NULL;
 
 /* Return line with given account or NUL if none.
  */
-struct line * find ( const char * account )
+line * find ( const char * account )
 {
-    struct line * result = lastline;
+    line * result = lastline;
     while ( result )
     {
         if ( strcmp ( result->account, account ) == 0 )
@@ -80,29 +140,69 @@ struct line * find ( const char * account )
 
 /* Insert line.
  */
-void insert ( const char * line, const char * des )
+void insert ( const char * linep, const char * des )
 {
-    struct line * newline
-        = (struct line *)
-	  malloc ( sizeof ( struct line ) );
-    char * p = malloc ( strlen ( line ) + 1 );
-    strcpy ( p, line );
+    line * newline =
+        (line *) malloc ( sizeof ( line ) );
+    char * p = malloc ( strlen ( linep ) + 1 );
+    strcpy ( p, linep );
+    INIT ( newline->rest, p );
     newline->account = p;
-    SKIP ( p );
-    newline->rest = p;
+    SKIP ( newline->rest );
     if ( find ( newline->account ) )
     {
         printf ( "ERROR: lines with duplicate account"
-	         " names in %s\n", des );
+	         " named %s in %s\n",
+		 newline->account, des );
 	exit ( 2 );
     }
     newline->previous = lastline;
     lastline = newline;
 }
     
-static char buffer [10000];
+/* Line buffer and function to read into it.
+ * Returns NULL on EOF like fgets.  fname is
+ * for error message.
+ */
+static char buffer [10002];
+char * read ( FILE * f, const char * fname )
+{
+    char * result =
+        fgets ( buffer, sizeof ( buffer ), f );
+    int length;
+    if ( result == NULL ) return result;
+    buffer[sizeof(buffer)-1] = 0;
+    length = strlen ( buffer );
+    if ( length > sizeof ( buffer ) - 2 )
+    {
+        printf ( "ERROR: line too long in %s\n",
+		 fname );
+	exit ( 2 );
+    }
+    assert ( length > 0 );
+    buffer[length-1] = 0;
+    return buffer;
+}
+
 int main ( int argc, char ** argv )
 {
+    int verbose;
+    const char * HOST;
+    const char * TYPE;
+    const char * command;
+    const char * file;
+    const char * source;
+    const char * target;
+    const char * tmpfile;
+    const char * src;
+    const char * des;
+    FILE * desf;
+    FILE * srcf;
+    /* FILE * tmpf;	This is global. */
+    int isput;
+    int ispasswd;
+    int hostlength;
+
     if ( argc < 9 )
     {
     	printf ( "conf_passwd_shadow VERBOSE HOST"
@@ -111,23 +211,21 @@ int main ( int argc, char ** argv )
 	exit ( 2 );
     }
 
-    int verbose          = ( argv[1][0] == '1' );
-    const char * HOST    = argv[2];
-    const char * TYPE    = argv[3];
-    const char * command = argv[4];
-    const char * file    = argv[5];
-    const char * source  = argv[6];
-    const char * target  = argv[7];
-    const char * tmpfile = argv[8];
+    verbose = ( argv[1][0] == '1' );
+    HOST    = argv[2];
+    TYPE    = argv[3];
+    command = argv[4];
+    file    = argv[5];
+    source  = argv[6];
+    target  = argv[7];
+    tmpfile = argv[8];
     
 #   define vprintf if ( verbose ) printf
 
-    /* Figure out which is the source file for the
-     * copy (src) and which is the ultimate
-     * destination (des) that the tmpfile is replace.
+    /* Figure out which is the source file (src) for the
+     * copy and which is the ultimate destination file
+     * (des) that the tmpfile is to replace.
      */
-    const char * src = NULL;
-    const char * des = NULL;
     if ( strcmp ( command, "put" ) == 0 )
     {
         src = source;
@@ -139,66 +237,65 @@ int main ( int argc, char ** argv )
     }
 
     /* Read the des file and insert its lines into the
+     * line list.
      */ 
-    FILE * desf = fopen ( des, "r" );
+    desf = fopen ( des, "r" );
     if ( desf == NULL )
     {
         printf ( "ERROR: cannot open %s for reading\n",
 		 des );
 	exit ( 2 );
     }
-    while ( fgets ( buffer, sizeof ( buffer ), desf ) )
-    {
-        buffer[strlen(buffer)-1] = 0;
+    while ( read ( desf, des ) )
         insert ( buffer, des );
-    }
     fclose ( desf );
 
     /* Copy src to tmpfile inserting fields as necessary
      * from des.
      */
-    FILE * srcf = fopen ( src, "r" );
+    srcf = fopen ( src, "r" );
     if ( srcf == NULL )
     {
         printf ( "ERROR: cannot open %s for reading\n",
 		 src );
 	exit ( 2 );
     }
-    FILE * tmpf = fopen ( tmpfile, "w" );
+    tmpf = fopen ( tmpfile, "w" );
     if ( tmpf == NULL )
     {
         printf ( "ERROR: cannot open %s for writing\n",
 		 tmpfile );
 	exit ( 2 );
     }
-    int isput = ( strcmp ( command, "put" ) == 0 );
-    int ispasswd = ( strcmp ( TYPE, "passwd" ) == 0 );
-    int hostlength = strlen ( HOST );
-    while ( fgets ( buffer, sizeof ( buffer ), srcf ) )
+    isput = ( strcmp ( command, "put" ) == 0 );
+    ispasswd = ( strcmp ( TYPE, "passwd" ) == 0 );
+    hostlength = strlen ( HOST );
+    while ( read ( srcf, src ) )
     {
-        buffer[strlen(buffer)-1] = 0;
-        char * p = buffer;
+	line * desline;
+        fpointer p;
+	INIT ( p, buffer );
 	SKIP ( p );
-	struct line * desline = find ( buffer );
+	desline = find ( buffer );
+#	define q desline->rest
+
+	/* p points at src field, q at des field */
 
 	if ( ispasswd )
 	{
 	    if ( desline == NULL )
 	    {
-		if ( * p )
-		    fprintf ( tmpf, "%s:%s\n",
-		              buffer, p );
-		else
-		    fprintf ( tmpf, "%s\n", buffer );
-
+		fprintf ( tmpf, "%s", buffer );
+		PRINTREST ( p );
 		continue;
 	    }
+
 	    if ( isput )
 	    {
-		/* shell comes from des file.
-		 */
-		char * q = desline->rest;
-		fprintf ( tmpf, buffer );
+		/* account */
+		fprintf ( tmpf, "%s", buffer );
+
+		/* 5 fields from src (source) */
 		PRINT ( p );
 		SKIP  ( q );
 		PRINT ( p );
@@ -210,17 +307,19 @@ int main ( int argc, char ** argv )
 		PRINT ( p );
 		SKIP  ( q );
 
+		/* shell from des (target) */
 		SKIP  ( p );
 		PRINT ( q );
 
-		fprintf ( tmpf, "%s\n", p );
+		/* rest of field from src (source) */
+		PRINTREST ( p );
 	    }
 	    else
 	    {
-		/* shell comes from src file.
-		 */
-		char * q = desline->rest;
-		fprintf ( tmpf, buffer );
+		/* account */
+		fprintf ( tmpf, "%s", buffer );
+
+		/* 5 fields from des (source) */
 		PRINT ( q );
 		SKIP  ( p );
 		PRINT ( q );
@@ -232,10 +331,12 @@ int main ( int argc, char ** argv )
 		PRINT ( q );
 		SKIP  ( p );
 
+		/* shell from src (target ) */
 		SKIP  ( q );
 		PRINT ( p );
 
-		fprintf ( tmpf, "%s\n", q );
+		/* rest of field from des (source) */
+		PRINTREST ( q );
 	    }
 	}
 	else /* is shadow */
@@ -250,11 +351,11 @@ int main ( int argc, char ** argv )
 	    int inhostlist = 0;
 	    int firsthost = 0;
 	    char * h;
-	    if ( isput ) h = p;
-	    else if ( desline == NULL ) h = "";
-	    else h = desline->rest;
-
 	    int first = 1;
+	    if ( isput ) h = p.cp;
+	    else if ( desline == NULL ) h = "";
+	    else h = q.cp;
+
 	    if ( * h == '!' ) while ( 1 )
 	    {
 	        char * k = ++ h;
@@ -263,7 +364,8 @@ int main ( int argc, char ** argv )
 		if ( k == h ) break;
 		if ( k - h == hostlength
 		     &&
-		     strncmp ( h, HOST, hostlength ) )
+		     strncmp ( h, HOST, hostlength )
+		     == 0 )
 		{
 		    if ( first ) firsthost = 1;
 		    inhostlist = 1;
@@ -273,60 +375,73 @@ int main ( int argc, char ** argv )
 		h = k;
 	    }
 
-
 	    if ( desline == NULL )
 	    {
-		fprintf ( tmpf, "%s:", buffer );
+		/* account */
+		fprintf ( tmpf, "%s", buffer );
+
 	        if ( isput )
 		{
+		    /* password */
 		    if ( ! hostlistexists )
-			fprintf ( tmpf, "%s\n", p );
+			PRINT ( p );
 		    else if ( inhostlist )
-			fprintf ( tmpf, "%s\n", h );
-		    else
+		    {
+			SKIP ( p );
+			fprintf ( tmpf, ":%s", h );
+		    }
+		    else /* not in existing host list */
 		    {
 		    	SKIP ( p );
-			fprintf ( tmpf, "!!:%s", p );
+			fprintf ( tmpf, ":!!%s", h );
 		    }
+		    PRINTREST ( p );
 		}
 		else /* is get */
 		{
 		    assert ( ! hostlistexists );
-		    fprintf ( tmpf, "!%s!%s\n",
-		                    HOST, p );
+		    h = p.cp;
+		    SKIP ( p );
+		    fprintf
+		        ( tmpf, ":!%s!%s", HOST, h );
+		    PRINTREST ( p );
 		}
 		continue;
 	    }
 
 	    /* account */
-
 	    fprintf ( tmpf, "%s", buffer );
+
 	    if ( isput )
 	    {
-		char * q = desline->rest;
-
 		/* password */
 		if ( firsthost )
 		{
-		    p = h;
+		    /* password from des (target) */
 		    PRINT ( q );
 		    SKIP ( p );
 		}
 		else if ( inhostlist )
 		{
-		    p = h;
+		    /* password minus hostlist
+		     * from src (source) */
+		    p.cp = h;
 		    SKIP ( q );
 		    PRINT ( p );
 		}
 		else if ( hostlistexists )
 		{
-		    p = h;
+		    /* password minus hostlist
+		     * with !! prefix
+		     * from src (source) */
 		    SKIP ( q );
 		    SKIP ( p );
-		    fprintf ( tmpf, ":!!" );
+		    fprintf ( tmpf, ":!!%s", h );
 		}
 		else
 		{
+		    /* password
+		     * from src (source) */
 		    SKIP ( q );
 		    PRINT ( p );
 		}
@@ -334,16 +449,18 @@ int main ( int argc, char ** argv )
 		/* password change date */
 		if ( firsthost )
 		{
+		    /* from des (target) */
 		    PRINT ( q );
 		    SKIP ( p );
 		}
 		else
 		{
+		    /* from src (source) */
 		    SKIP ( q );
 		    PRINT ( p );
 		}
 
-		/* four fields copied from source */
+		/* 4 fields copied from src (source) */
 		PRINT ( p );
 		SKIP  ( q );
 		PRINT ( p );
@@ -356,33 +473,39 @@ int main ( int argc, char ** argv )
 		/* account disable date */
 		if ( firsthost )
 		{
+		    /* from des (target) */
 		    PRINT ( q );
 		    SKIP ( p );
 		}
 		else
 		{
+		    /* from src (source) */
 		    SKIP ( q );
 		    PRINT ( p );
 		}
 
-		fprintf ( tmpf, ":%s\n", p );
+		/* rest copied from src (source) */
+		PRINTREST ( p );
 	    }
 	    else /* is get */
 	    {
-		char * q = desline->rest;
-
 		/* password */
 		if ( firsthost )
 		{
-		    * h = ':';
-		    PRINT ( q );
+		    /* hostlist from des (source) */
+		    char save = * h;
+		    * h = 0;
+		    fprintf ( tmpf, ":%s", q.cp );
+		    * h = save;
 		    SKIP ( q );
-		    h = p;
+		    /* password from src (target) */
+		    h = p.cp;
 		    SKIP ( p );
 		    fprintf ( tmpf, "%s", h );
 		}
 		else
 		{
+		    /* password from des (source) */
 		    PRINT ( q );
 		    SKIP ( p );
 		}
@@ -390,16 +513,18 @@ int main ( int argc, char ** argv )
 		/* password change date */
 		if ( firsthost )
 		{
+		    /* from src (target) */
 		    PRINT ( p );
 		    SKIP ( q );
 		}
 		else
 		{
+		    /* from des (source) */
 		    SKIP ( p );
 		    PRINT ( q );
 		}
 
-		/* four fields copied from source */
+		/* 4 fields copied from des (source) */
 		SKIP  ( p );
 		PRINT ( q );
 		SKIP  ( p );
@@ -412,16 +537,19 @@ int main ( int argc, char ** argv )
 		/* account disable date */
 		if ( firsthost )
 		{
+		    /* from src (target) */
 		    PRINT ( p );
 		    SKIP ( q );
 		}
 		else
 		{
+		    /* from des (source) */
 		    SKIP ( p );
 		    PRINT ( q );
 		}
 
-		fprintf ( tmpf, ":%s\n", q );
+		/* rest from des ( source ) */
+		PRINTREST ( q );
 	    }
 	}
     }
