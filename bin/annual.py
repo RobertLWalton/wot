d3a4a9baa21362da559ee1d3863b4122b23dea68
Robert L. Walton <walton@acm.org>
2021-08-09 05:08:49 -0400

    update

--- bin/annual.py
+++ bin/annual.py
@@ -4,7 +4,7 @@
 #
 # File:         annual.py
 # Authors:      Bob Walton (walton@acm.org)
-# Date:         Sun Aug  8 21:49:36 EDT 2021
+# Date:         Mon Aug  9 05:08:35 EDT 2021
 #
 # The authors have placed this program in the public
 # domain; they make no warranty and accept no liability
@@ -12,7 +12,7 @@
 
 import sys
 import math
-
+import re
 
 document = """
 Program to Compute Annual Returns of an Investment
@@ -59,3 +59,74 @@ Output Format:
 if len ( sys.argv ) <= 1:
     print ( document )
     exit ( 1 )
+
+year_re = re.compile ( r"^\d\d\d\d$" )
+
+description = []
+values = {}
+year_line = {}
+
+filename = sys.argv[1]
+line = ""
+line_number = 0
+
+def fail_message ( message ):
+    print ( 'FATAL ERROR: ' + message + 
+            ' in line number ' + str ( line_number ) +
+            ':' + "\n" + line,
+            file = sys.stderr );
+
+def Fail ( message ):
+    fail_message ( message )
+    sys.exit ( 1 )
+
+try:
+    f = open ( filename )
+    description_done = False
+    while True:
+        line = f.readline()
+        if line == '': break
+        line_number += 1
+        line = line.rstrip()
+        if line == '': continue
+        if ( not description_done ):
+            if line[0] == '*':
+                description.append ( line )
+                continue
+            else:
+                description_done = True
+        line = line.strip()
+        pair = line.split()
+        if len ( pair ) != 2:
+            Fail ( "badly formatted data line" )
+        lyear = pair[0]
+        lvalue = pair[1]
+        match = year_re.match ( lyear )
+        if not match:
+            Fail ( lyear + " is not a 4-digit" +
+                          " year number" )
+        year = int ( lyear )
+        try:
+            value = float ( lvalue )
+        except:
+            Fail ( lvalue + " is not a floating" +
+                            " point number" )
+        if value <= 0:
+            Fail ( lvalue + " is negative or zero" )
+        if year_line.get ( year ):
+            Fail ( lyear + " is was in line " +
+                           str ( year_line[year] ) )
+        year_line[year] = line_number
+        values[year] = value
+        
+
+
+except FileNotFoundError:
+    print ( "FATAL ERROR: could not open " +
+            filename + " for reading" );
+    sys.exit ( 1 )
+except SystemExit:
+    raise
+except:
+    fail_message ( "exception raised" )
+    raise
