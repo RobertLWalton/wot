6c0161a775c606b38b7b5c08f58e7ca9de3bf3d9
Robert L. Walton <walton@acm.org>
2024-10-01 19:39:40 -0400

    update

--- perl/linerep.pl
+++ perl/linerep.pl
@@ -1,31 +1,34 @@
 # perl linerep.pl
 #
-# Copies the standard input to the standard output replacing all
-# instances of the $from lines by the $to lines.
+# Copies the standard input to the standard output
+# replacing all instances of the $from lines by
+# the $to lines.
 #
 #     lineref.pl, Bob Walton, walton@acm.org,
-#     Sun Sep 29 03:30:36 AM EDT 2024
+#     Tue Oct  1 07:22:23 PM EDT 2024
 
+use strict;
+use warnings;
 
-$FROM = $ARGV[0];
-$TO   = $ARGV[1];
+my $FROM = shift;
 
 if ( not defined $FROM )
 {
     print
 'perl linerep.pl
-perl linerep.pl FROM-FILE TO-FILE <TEXT-IN >TEXT-OUT
+perl linerep.pl FROM-FILE TO-FILE TEXT-FILE1 \
+                                  TEXT-FILE2 ...
 perl linerep.pl FROM-FILE
 
 When no FROM-FILE is given, this document is output.
 
-When TO-FILE is given, this program copies its standard
-input to its standard output replacing every instance
-of the FROM-FILE contents with the TO-FILE contents.
+When TO-FILE is given, this program modifies the
+TEXT-FILEs by replacing every occurance of the contents
+of FROM-FILE by the contents of TO-FILE.
 
 The contents of FROM-FILE must be a sequence of lines
 encoding tokens that do not contain whitespace and
-that are separated by whitespace.  Instances of the
+that are separated by whitespace.  Occurances of the
 FROM-FILE contents in the input text may have different
 whitespace characters than the FROM-FILE contents has;
 e.g., a space character may be replaced by a line-feed
@@ -41,14 +44,49 @@ given but TO-FILE is not, the standard input is ignored
 and the regular expression is copied to the standard
 output.  This is only useful for debugging.
 
+Example:
+
+FROM-FILE Contents:
+FOOBAR ABC
+    [ 99 ]
+
+TO-FILE Contents:
+FOOBAR
+ABC
+  [ 88 ]
+
+TEXT-FILE Contents BEFORE Modification:
+FOOBAR
+[
+    ABC [ 22 ]
+]
+FOOBAR ABC [ 99 ]
+FOOBAR ABC [ 33 ]
+FOOBAR
+   ABC [ 99 ]
+THE END
+
+TEXT-FILE Contents AFTER Modification:
+FOOBAR
+[
+    ABC [ 22 ]
+]
+FOOBAR
+ABC
+  [ 88 ]
+FOOBAR ABC [ 33 ]
+FOOBAR
+ABC
+  [ 88 ]
+THE END
 ';
     exit ( 0 );
 }
 
 $/ = undef;
-open ( from_handle, $FROM )
-    or die ( "cannot read $FROM" );
-$from = <from_handle>;
+open ( my $from_handle, $FROM )
+    or die ( "cannot read $FROM: $!" );
+my $from = <$from_handle>;
     # read $FROM file into $from
 
 $from =~ s/^\s+|\s+$//g;
@@ -57,26 +95,41 @@ $from = quotemeta ( $from );
     # put \ just before all space and [ characters
     # and other regular expression metacharacters
 $from =~ s/(\\[ \n\t])+/\\s+/g;
-    # replace all whitespace (now with \'s before) by the
-    # regular subexpression \s+
+    # replace all whitespace (now with \'s before) by
+    # the regular subexpression \s+
 $from = '[\ \t]*' . $from . '[\ \t\r]*[\n]';
     # include any whitespace at the beginning of the
     # first $from instance line and optional whitespace
     # followed by \r\n or \n at the end of the instance.
 
+my $TO   = shift;
 if ( not defined $TO )
 {
     print "$from\n";
     exit ( 0 );
 }
 
-open ( to_handle, $TO )
-    or die ( "cannot read $TO" );
-$to = <to_handle>;
+open ( my $to_handle, $TO )
+    or die ( "cannot read $TO: $!" );
+my $to = <$to_handle>;
     # read $TO file into $to
-$text = <STDIN>;
-   # read standard input into the $text variable
-   #
-$text =~ s/$from/$to/g;
-    # replace $from by $to in $text
-print "$text";
+
+
+my $TEXT = shift;
+while ( defined $TEXT )
+{
+    open ( my $text_handle, $TEXT )
+	or die ( "cannot read $TEXT: $!" );
+    my $text = <$text_handle>;
+    close $text_handle;
+
+    $text =~ s/$from/$to/g;
+	# replace $from by $to in $text
+
+    open ( $text_handle, '>', $TEXT )
+	or die ( "cannot write $TEXT: $!" );
+    print $text_handle "$text";
+    close $text_handle;
+
+    $TEXT = shift;
+}
